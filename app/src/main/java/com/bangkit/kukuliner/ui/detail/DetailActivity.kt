4285dccbf323fcc832c8574c2c27f0288555dffc
Xeismonium <alfan.ghifari23@gmail.com>
2024-05-26 07:31:41 +0700

    Removing enableEdgeToEdge()

--- app/src/main/java/com/bangkit/kukuliner/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/bangkit/kukuliner/ui/detail/DetailActivity.kt
@@ -19,7 +19,6 @@ class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_detail)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
