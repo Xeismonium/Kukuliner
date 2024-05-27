dfe970a2a3488a0bd16dc037e1467d09958073e4
Xeismonium <alfan.ghifari23@gmail.com>
2024-05-27 16:55:06 +0700

    Remove duplicate codes in Detail, Add Locale

--- app/src/main/java/com/bangkit/kukuliner/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/bangkit/kukuliner/ui/detail/DetailActivity.kt
@@ -27,15 +27,13 @@ class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        binding = ActivityDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
         viewModel.getThemeSettings().observe(this) { isDarkModeActive: Boolean ->
             if (isDarkModeActive) {
